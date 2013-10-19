0c69eb97d4e1a0dc5495b007802e94753c108ada
Andrew <Andrew@10.18.147.184>
2013-10-19 12:37:34 -0600

    splash class

---
+++ src/manhang/splash.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manhang;
+
+/**
+ *
+ * @author Andrew
+ */
+public class splash {
+    
+     public static void splashScrene(){
+        System.out.println("***********************************************************************"); 
+        System.out.println("*                                                __                   *"); 
+        System.out.println("*   |\\    /|   /\\   |\\  |   |   |   /\\   |\\  |  /  \\                 *"); 
+        System.out.println("*   | \\  / |  /--\\  | \\ |   |---|  /--\\  | \\ | |  __                  *"); 
+        System.out.println("*   |  \\/  | /    \\ |  \\|   |   | /    \\ |  \\|  \\__/                  *"); 
+        System.out.println("*                                                                     *"); 
+        System.out.println("*                          A hangman game.                            *"); 
+        System.out.println("*                                                                     *");  
+        System.out.println("*         1-4 players, Easy, Medium, Hard difficulty                  *");
+        System.out.println("*               Guess a letter, find the word.                        *"); 
+        System.out.println("***********************************************************************");   
+        
+    }
+    
+    
+}
