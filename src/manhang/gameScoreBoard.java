94d02611366c88fc721142f772bb20d398fbda82
Steve <Steve@10.18.151.206>
2013-10-21 12:25:49 -0600

    fixed comment errors

--- src/manhang/gameScoreBoard.java
+++ src/manhang/gameScoreBoard.java
@@ -10,4 +10,10 @@ package manhang;
  */
 public class gameScoreBoard {
     
-}
+    public static void wordStatus (char wLet1, char wLet2, char wLet3, char wLet4,
+            char wLet5, char wLet6, char wLet7, char wLet8, char wLet9, char wLet10, 
+            char wLet11, char wLet12, char wLet13,char wLet14,char wLet15,char wLet16){
+         System.out.println("Word:");
+          System.out.println(wLet1 + " " + wLet2 + " " + wLet3 + " " + wLet4 + " " + wLet5 + " " + wLet6 + " " + wLet7 + " " + wLet8 + " " + wLet9 + " " + wLet10 + " " + wLet11 + " " + wLet12 + " " + wLet13 + " " + wLet14 + " " + wLet15 + " " + wLet16);
+    
+}}
