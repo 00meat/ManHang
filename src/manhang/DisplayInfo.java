4f0e5478acfc60bf60169db614edfb6925555ec4
Andrew <Andrew@10.18.147.184>
2013-11-30 12:26:55 -0700

    working on interfaces

---
+++ src/manhang/DisplayInfo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manhang;
+
+/**
+ *
+ * @author Andrew
+ */
+public interface DisplayInfo {
+    
+    implements gameboard.scoreboard(score, guessRight, guessWrong);
+    
+    
+}
---
+++ src/manhang/EnterInfo.java
@@ -0,0 +1,14 @@
+package manhang;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andrew
+ */
+public interface EnterInfo {
+    
+}
