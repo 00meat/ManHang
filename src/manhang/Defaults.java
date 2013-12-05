ab15d358cea7067b1ea38aa3bc14c5321a442a54
Andrew <Andrew@10.18.147.184>
2013-12-05 11:36:31 -0700

    work danget

---
+++ src/manhang/Defaults.java
@@ -0,0 +1,19 @@
+package manhang;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andrew
+ */
+public enum Defaults {
+    //The program dosn't really have any differient states 
+    //that are static and final that can be defined here.
+    //but lets try it anyway.
+    Win (0),
+    Loss (0);
+    //
+}
