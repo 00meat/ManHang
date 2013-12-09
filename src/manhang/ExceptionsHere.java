f8f3840be0f03b8f99e5c11ee76ee784b21eaa7e
Steve <Steve@10.18.151.206>
2013-12-09 11:55:59 -0700

    win loss errors

---
+++ src/manhang/ExceptionsHere.java
@@ -0,0 +1,30 @@
+package manhang;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Andrew
+ */
+public class ExceptionsHere extends Exception {
+
+    /**
+     * Creates a new instance of
+     * <code>ExceptionsHere</code> without detail message.
+     */
+    public ExceptionsHere() {
+    }
+
+    /**
+     * Constructs an instance of
+     * <code>ExceptionsHere</code> with the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public ExceptionsHere(String msg) {
+        super(msg);
+    }
+}
